8a95ed8b3ba3f263e7a4ac8b8ce9d3ca3f036f0f
ganushkevychv <ganushkevychv@gmail.com>
2021-02-15 18:15:15 +0100

    project created, header and slogan added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="containerApp">
+      <header className="header">
+       <h1 className="headerTitle">Unsplash</h1>
+       <p className="headerSlogan">The internet's source of<a href="https://unsplash.com/license"> freely-usable images.</a>Powered by creators everywhere.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
