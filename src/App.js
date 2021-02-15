d3c7f5557ebc46d36724e4bb2234dfab25d8acbd
ganushkevychv <ganushkevychv@gmail.com>
2021-02-15 20:34:51 +0100

    front end part of main page done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
-import searchIcon from '../src/img/search.png'
+import searchIcon from '../src/img/search.png';
 
 
 function App() {
+
+  const formStyle = {
+    backgroundColor: "white",
+    fontSize: "20px",
+    borderRight:"none"
+  }
+  const inputStyle = {
+    borderLeft:"none"
+  }
+
   return (
     <div className="containerApp">
       <header className="header">
        <h1 className="headerTitle">Unsplash</h1>
-       <p className="headerSlogan">The internet's source of<a href="https://unsplash.com/license"> freely-usable images.</a>Powered by creators everywhere.</p>
+       <p className="headerSlogan">The internet's source of<a href="https://unsplash.com/license"> freely-usable images.</a><br/>Powered by creators everywhere.</p>
        <InputGroup>
         <InputGroupAddon addonType="prepend">
-          <InputGroupText><img className="searchIcon" src={searchIcon}></img></InputGroupText>
+          <InputGroupText style={formStyle}><img className="searchIcon" src={searchIcon} alt="loupe icon"></img></InputGroupText>
         </InputGroupAddon>
-        <Input placeholder="Search free high-resolution photos" />
+        <Input style={inputStyle} size="100" placeholder="Search free high-resolution photos" />
       </InputGroup>
-      <p className="tranding">Tranding: flower, wallpapers, backgrounds, happy, love</p>
+      <p className="headerTrending">Trending: flower, wallpapers, backgrounds, happy, love</p>
       </header>
     </div>
   );
