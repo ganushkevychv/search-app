6670a2a7194facb37e895aef4832708ec9e9fe2c
ganushkevychv <ganushkevychv@gmail.com>
2021-02-15 18:58:09 +0100

    reactstrap installed to add form to header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import searchIcon from '../src/img/search.png'
+
 
 function App() {
   return (
@@ -7,6 +11,13 @@ function App() {
       <header className="header">
        <h1 className="headerTitle">Unsplash</h1>
        <p className="headerSlogan">The internet's source of<a href="https://unsplash.com/license"> freely-usable images.</a>Powered by creators everywhere.</p>
+       <InputGroup>
+        <InputGroupAddon addonType="prepend">
+          <InputGroupText><img className="searchIcon" src={searchIcon}></img></InputGroupText>
+        </InputGroupAddon>
+        <Input placeholder="Search free high-resolution photos" />
+      </InputGroup>
+      <p className="tranding">Tranding: flower, wallpapers, backgrounds, happy, love</p>
       </header>
     </div>
   );
