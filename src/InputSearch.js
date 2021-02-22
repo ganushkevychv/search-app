aecdd50fad771a5b9ff2482570671bbc87365cde
ganushkevychv <ganushkevychv@gmail.com>
2021-02-22 11:48:07 +0100

    autocomplete option added

--- src/InputSearch.js
+++ src/InputSearch.js
@@ -2,20 +2,25 @@ import React, {useState} from 'react';
 import './App.css';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import searchIcon from '../src/img/search.png';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const InputSearch = ({value, valueHandler}) => {
-  const autoComplete = [
-    "apple",
-    "apricot",
-    "banana",
-    "carrot",
-    "unicorn",
-    "cat",
-    "island",
-    "beach",
-    "sunrise",
-    "sunset", 
-  ];
+
+  
+  const top100Films = [
+    { title: 'cat'},
+    { title: 'island'},
+    { title: 'cats'},
+    { title: 'sunrise'},
+    { title: 'sunset'},
+    { title: "beach"},
+    { title: 'animals'},
+    { title: 'kids'},
+    { title: 'people'},
+    { title: 'office'},
+   
+  ]
 
   const formStyle = {
     backgroundColor: "white", 
@@ -34,6 +39,9 @@ const handleEnterKey = (event) => {
     valueHandler(event.target.value)
   }
 }
+const handleMouseClick = (event) => {
+    valueHandler(event.target.value)
+}
 
   const onChange = (e) => {
     const {
@@ -47,17 +55,33 @@ return(
      <div className="containerApp">
        <h1 className="headerTitle">Unsplash</h1>
        <p className="headerSlogan">The internet's source of<a className="headerSloganLink" href="https://unsplash.com/license"> freely-usable images.</a><br/>Powered by creators everywhere.</p>
-       <InputGroup size="sm">
+       <div style={{ width: 300 }}>
+      <Autocomplete
+        freeSolo
+        
+        disableClearable
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+          onChange={onChange}
+          onKeyDown={handleEnterKey}
+          onClick={handleMouseClick}
+            {...params}
+            label="Search input"
+            margin="normal"
+            variant="outlined"
+            InputProps={{ ...params.InputProps, type: 'search' }}
+          />
+        )}
+      />
+    </div>
+       {/* <InputGroup size="sm">
        <InputGroupAddon addonType="prepend">
       <InputGroupText style={formStyle}><img className="searchIcon" src={searchIcon} alt="loupe icon"></img></InputGroupText>
     </InputGroupAddon>
-    <Input onChange={onChange} onKeyDown={handleEnterKey} option={autoComplete} type="text" style={inputStyle} placeholder="Search free high-resolution photos"/>
-    </InputGroup>
+    <Input onChange={onChange} onKeyDown={handleEnterKey}  type="text" style={inputStyle} placeholder="Search free high-resolution photos"/>
+    </InputGroup> */}
       <p className="headerTrending">Trending: flower, wallpapers, backgrounds, happy, love</p>
-      
-    
-    
-    
   </div>
   </div>
 )
